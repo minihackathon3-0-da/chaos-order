3c6b3be6e9fc8c135f673b47dd21ba47ee159d7f
Noureddin Boussaada <n.boussaada92@gmail.com>
2025-12-07 18:16:47 +0100

    template header footer

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,19 +1,3 @@
-// function getHeaderTemplate() {
-//     return `
-//         <img class="img-logo" src="./assets/icons/1764709167_ec700f188158429b9809da4d628544a2.png" alt="Logo">
-//         <a href="#home" class="active">Logo</a>
-//         <button id="burger_menu">≡</button>
-//     `;
-// }
-
-
-// function getFooterTemplate(){
-//     return `   
-//         <span>&copy; 2025 Chaos-Order</span>
-//         <span>developed by</span>
-//         <span>Chaos-Order Team</span>
-//         `;
-// }
 
 export function getHeaderTemplate() {
     return `
