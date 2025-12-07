83da187d05d991aeb5c8de508be2b2f44cb5571a
Aleks <143895611+A1eksD@users.noreply.github.com>
2025-12-07 17:23:08 +0100

    changed color fucntion and added bord shadow

--- features/board/boards.js
+++ features/board/boards.js
@@ -121,10 +121,10 @@ function makeCardDraggable(card) {
 }
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+  const letters = '89ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * letters.length)];
   }
   return color;
 }
